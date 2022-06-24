099bacbb90c0196a5bb7fbf8b295ca4628c4d471
itsnico01 <n1c0undso0901@gmail.com>
2022-06-24 16:24:47 +0200

    push

--- Ave-Code-Dado/client/src/components/AppMealsFilter/AppMealsFilter.js
+++ Ave-Code-Dado/client/src/components/AppMealsFilter/AppMealsFilter.js
@@ -3,16 +3,17 @@ import React from "react";
 const AppMealsFilter = ({setSelectedFilter,selectedFilter}) => {
     return (
         <div className="app__meals__container__select">
-            <select defaultValue={selectedFilter}
-            onChange={(e)} => setSelectedFilter(e-target.value)}
-                >
-                <option value=""></option>
-                <option value="Ascending">Ascending</option>
-                <option value="Descending">Descending</option>
+            <select
+            defaultValue={selectedFilter}
+            onChange={(e) => setSelectedFilter(e.target.value)}
+            >
+              <option value=""></option>
+              <option value="Ascending">Ascending</option>  
+              <option value="Descending">Descanding</option>  
             </select>
         </div>
     )
 }
 
 
-export default AppMealsFilter
+export default AppMealsFilter;
--- Ave-Code-Dado/client/src/components/AppMealsList/AppMealsList.js
+++ Ave-Code-Dado/client/src/components/AppMealsList/AppMealsList.js
@@ -2,20 +2,24 @@ import React from 'react'
 
 const AppMealsList = ({meals, deleteMealHandler}) => {
 
-
-    return (
-        <div className="app_meals_container_wrapper">
-            {meals.map((meals, index) => (
-               <div key=index className="app_meals_container_wrapper_inner">
-                    <div>(´${meal.mealName} : ${meal.calories}´)</div>
-                   <div>
-                       <button className="btn_delete_meal" onClick=()=>{deleteMealHandler(meal.id)}>Löschen</button>
-                   </div>
-                </div>
-                ))}
-
+return (
+<div className="app__meals__container__wrapper">
+    {meals.map((meal, index)=> (
+        <div key={index} className="app__meals__container__wrapper__inner">
+            <div>{`${meal.mealName} : ${meal.calories}`} </div>
+            <div>
+                <button className="btn__delete__meal"
+                        onClick={()=>deleteMealHandler(meal.id)}
+                        > Löschen</button>
+            </div>
         </div>
+    ))}
+</div>
+
+   
     )
+    
+
 }
 
 export default AppMealsList;
