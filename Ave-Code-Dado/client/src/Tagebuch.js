16ceb6c7ce1029df0356e6429c8aafeb9d729daa
Sabrina <82584932+Nifflli@users.noreply.github.com>
2022-07-01 16:46:56 +0200

    update calendar Tagebuch - not ready

--- Ave-Code-Dado/client/src/Tagebuch.js
+++ Ave-Code-Dado/client/src/Tagebuch.js
@@ -1,21 +1,60 @@
-import React, { Component } from "react";
+import React, {Component, useState} from "react";
 import Header from "./Header";
 import {BrowserView, MobileView} from 'react-device-detect';
 import HeaderMobile from "./HeaderMobile";
+import 'date-fns'
+import {Grid} from "@material-ui/core";
+import DateFnsUtils from '@date-io/date-fns'
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboarddatePicker
+} from '@material-ui/pickers'
 
-class Tagebuch extends Component {
-  render() {
-    return (
-      <div>
-              <BrowserView>
-          <Header />
-        </BrowserView>
-        <MobileView>
-          <HeaderMobile />
-        </MobileView>
-      </div>
-    );
-  }
+
+function Tagebuch() {
+
+    const [selectedDate, setSelectedDate] = useState(
+        new Date("2022-07-01")
+    )
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date)
+    }
+
+
+        return (
+            <div>
+                <div className="diary-calendar">
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <Grid container justify='space-around'>
+                            <KeyboarddatePicker
+                                disableToolbar
+                                variant='dialog'
+                                format='MM/dd/yy'
+                                margin='normal'
+                                id='date-picker'
+                                label='Date Picker'
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{
+                                    'aria-label' : 'change date'
+                                }}
+                            />
+                        </Grid>
+                    </MuiPickersUtilsProvider>
+
+                </div>
+                <BrowserView>
+                    <Header/>
+                </BrowserView>
+                <MobileView>
+                    <HeaderMobile/>
+                </MobileView>
+            </div>
+        );
+
+
 }
 
 export default Tagebuch;
